35b071e973c9c649e20c67c2f8c56220d3ed79f3
Hassan Irtaza <122802631+HassanIrtaza@users.noreply.github.com>
2023-02-07 13:56:20 +0500

    Update before_install.sh

--- models/Scripts/before_install.sh
+++ models/Scripts/before_install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 # navigate to app folder
-cd /home/ubuntu
-curl -fsSL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
-bash /tmp/nodesource_setup.sh
-apt-get install -y nodejs
-npm i -g pm2
+cd cd /home/ubuntu/demoNodeProject
 #npm install pm2 -g
